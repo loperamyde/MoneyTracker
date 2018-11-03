9c864ecac368beb77146d86335679bb6e133d3d1
user <loperamude@gmail.com>
2018-11-03 16:21:09 +0200

    text color end size

--- app/src/main/java/comviewnickapps/google/httpssites/moneytracker/MainActivity.java
+++ app/src/main/java/comviewnickapps/google/httpssites/moneytracker/MainActivity.java
@@ -2,6 +2,9 @@ package comviewnickapps.google.httpssites.moneytracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MainActivity","onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivity","onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity","onStart");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivity","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivity","onDestroy");
     }
 }
